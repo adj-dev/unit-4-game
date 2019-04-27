8b3446174b5e26830002ddef28298658a549789d
adj-dev <andrew.johnson2073@gmail.com>
2019-04-27 14:25:21 -0500

    Defined character classes

---
+++ assets/javascript/game.js
@@ -0,0 +1,32 @@
+class Skywalker {
+  constructor() {
+    this.name = 'Luke Skywalker';
+    this.hp = 100;
+    this.attackPoints = 14;
+  }
+}
+
+class Kenobi {
+  constructor() {
+    this.name = 'Obi-Wan Kenobi';
+    this.hp = 120;
+    this.attackPoints = 8;
+  }
+}
+
+class Solo {
+  constructor() {
+    this.name = 'Han Solo';
+    this.hp = 150;
+    this.attackPoints = 6;
+  }
+}
+
+class Darth Maul {
+  constructor() {
+    this.name = 'Darth Maul';
+    this.hp = 180;
+    this.attackPoints = 20;
+  }
+}
+
